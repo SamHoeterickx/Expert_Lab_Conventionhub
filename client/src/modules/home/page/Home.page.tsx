c5a23aa6ed069e24087f76bad8eb532824c17f8a
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-11-04 14:21:13 +0100

    Feat(home): Added slogan

--- client/src/modules/home/page/Home.page.tsx
+++ client/src/modules/home/page/Home.page.tsx
@@ -135,6 +135,23 @@ export const Home: FC = () => {
                         </div>
                     </div>
                 </section>
+
+                <section className="slogan-section">
+                    <div className="left-wrapper">
+                        <div className="left-container">
+                            <div className="left-content">
+                                <h2>YOUR</h2>
+                                <h2>CONVENTION</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right-wrapper">
+                        <div className="right-content">
+                            <h2>CENTRAL</h2>
+                            <h2>HUB</h2>
+                        </div>
+                    </div>
+                </section>
             </div>
         </>
     );
