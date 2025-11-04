591c185901dae116fc713543f8cc221d1c8f17d3
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-11-04 09:32:59 +0100

    Feat(styling): Added fonts, font faces, base styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Root } from './modules/root'
 
+//Styles
+import './styles/base.css';
+import './styles/variables.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
