c8d6a7b13cc6ec0c4e1592df5f2c7c30e6acbb28
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-11-04 14:32:23 +0100

    Feat(shared/footer): Added content and styling to footer

--- client/src/shared/components/footer/Footer.tsx
+++ client/src/shared/components/footer/Footer.tsx
@@ -1,5 +1,49 @@
+//Style
+import { Link } from 'react-router-dom';
+import './footer.css';
+
 export const Footer = () => {
     return (
-        <footer></footer>
+        <footer>
+            <div className="footer-container">
+                <div className="left-wrapper">
+                    <ul>
+                        <li>
+                            <Link to={"#"}>
+                                <p>Explore Conventions</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={"#"}>
+                                <p>Create Conventions</p>
+                            </Link>
+                        </li>
+                        <li><p>
+                                2025
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="right-wrapper">
+                    <ul>
+                        <li>
+                            <Link to={"#"}>
+                                <p>Cookiepolicy</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={"#"}>
+                                <p>Privacypolicy</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={"#"}> 
+                                <h4>STANDARDSHUB</h4>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     )
 }
