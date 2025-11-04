fb483e34e838a01766bc89e50272a46e6c05948c
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-11-04 09:13:47 +0100

    feat(shared/components): Added barrel file for shared components, added footer to App file

--- client/src/modules/app/App.tsx
+++ client/src/modules/app/App.tsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router-dom"
 
+//Components
+import { Footer } from "../../shared/components"
+
 export const App = () => {
     return (
-        <Outlet />
+        <>
+            <Outlet />
+            <Footer />
+        </>
     )
 }
