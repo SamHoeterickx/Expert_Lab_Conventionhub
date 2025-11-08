4641311c04018ef56208f38bdfe0f53c95bbae00
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-11-08 11:46:34 +0100

    Feat(client/Header): Created shared header component

---
+++ client/src/shared/components/header/Header.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState, type FC } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
+
+//Components
+import { FilterDropDown } from "./components/filterDropDown/FilterDropDown";
+
+//Style
+import './header.css'
+
+//Type
+interface HeaderProps {
+    title: string
+}
+
+export const Header: FC<HeaderProps> = ({ title }) => {
+
+    const [currentPage, setCurrentPage] = useState<string | undefined>(undefined);
+    const [isFilterState, setIsFilterState] = useState<boolean>(false);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const location = useLocation();
+
+    useEffect(() => {
+        const pathName = location.pathname;
+        const pageParam = pathName.split('/');
+        const fsParam = searchParams.get('fs');
+
+        setCurrentPage(pageParam[1]);
+
+        if(fsParam === 'true'){
+            setIsFilterState(true);
+        }else {
+            setIsFilterState(false);
+        }
+
+    }, [location]);
+
+    const handleSearchClick = () => {
+        const filterState = !isFilterState;
+        
+        setIsFilterState(!isFilterState);
+        setSearchParams({
+            s: 'hey', 
+            fs: filterState.toString()
+        })
+    }
+
+    return (
+        <div className="header-component-wrapper">
+            <div className="header-component-inner-wrapper">
+                <h2>{ title }</h2>
+                {
+                    currentPage === "explore" && (
+                        <button
+                            onClick={ handleSearchClick }
+                        >
+                            🔍
+                        </button>
+                    )
+                }
+                {
+                    isFilterState && <FilterDropDown />
+                }
+            </div>
+        </div>
+    )
+}
