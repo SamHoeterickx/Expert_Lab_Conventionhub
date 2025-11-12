915d73c598d8e6aedbb7bfc40dbaa746606bcbeb
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-11-12 16:57:16 +0100

    Feat(convention): WIP Convention page

--- client/src/modules/conventions/page/Convention.page.tsx
+++ client/src/modules/conventions/page/Convention.page.tsx
@@ -1,58 +1,90 @@
-import { Link, useParams } from "react-router-dom"
+import { type FC } from "react";
+import { Link, useParams } from "react-router-dom";
+// import Markdown from "markdown-to-jsx";
+import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-import { useEffect, type FC } from "react";
 
 //Components
-import { Header, PreFooter } from "../../../shared/components"
+import { Header, PreFooter } from "../../../shared/components";
 
 //Hooks
 import { useGetSingleConvention } from "../../../shared/hooks";
 
 //Style
-import './Convention.css'
+import './convention.css';
 
 
-export const Convention:FC = () => {
-    
+export const Convention: FC = () => {
+
     const { slug } = useParams();
 
-    const {data, isLoading, isError, error} = useGetSingleConvention(slug);
+    const { data, isLoading, isError, error } = useGetSingleConvention(slug);
 
-    useEffect(() => {
-        console.log(data?.data)
-    }, [data]);
 
-    return( 
+    const processedMd = data?.data?.contentMd
+        ? data.data.contentMd
+            // .replace(/\\n/g, "\\n")   // Vervang de TEKST '\\n' door een ECHTE newline
+            // .replace(/\\`/g, "`")    // Vervang de TEKST '\\`' door een ECHTE backtick
+            // .replace(/\\'/g, "'")    // Vervang de TEKST '\\'' door een ECHTE single quote
+            // .replace(/\\\${/g, '${') // Vervang de TEKST '\\${' door een ECHTE template literal
+        : ""
+    ;
+
+    console.log(processedMd);
+
+    return (
         <>
             {
-                isLoading && <h2>LOADING...</h2>
-            }
-            {
-                data && data.data &&
-                <>
-                    <Header title={ data.data.title.toUpperCase()} />
-                    <div className="convention-info-wrapper">
-                        <div className="convention-right-corner-container">
-                            <div className="convention-right-corner"></div>
-                        </div>
-                        <div className="convention-markdown-wrapper markdown-body">
-                            <ReactMarkdown remarkPlugins={[remarkGfm]}>{data.data.contentMd}</ReactMarkdown>
-                        </div>
-                    </div>
+                processedMd && (
+                    <>
+                        {
+                            data && data.data && <Header title={data.data.title.toUpperCase()} />
+                        }
+                        <div className="convention-info-wrapper">
+                            <div className="convention-right-corner-container">
+                                <div className="convention-right-corner"></div>
+                                </div>
+                            <div className="convention-markdown-wrapper">
+                                <Markdown
+                                    components={{
+                                        code({ node, className, children, ...props }) {
+                                        const match = /language-(\w+)/.exec(className || '');
+                                        return match ? (
+                                            <SyntaxHighlighter
+                                                style={oneDark}
+                                                language={match[1]}
+                                                PreTag="div"
+                                                {...props}
+                                            >
+                                                {String(children)}
+                                            </SyntaxHighlighter>
+                                        ) : (
+                                            <code className={className} {...props}>
+                                                {children}
+                                            </code>
+                                        );
+                                        }
+                                    }}
+                                    >
+                                    {data?.data?.contentMd}
+                                </Markdown>
 
-                    <PreFooter>
-                        <h2 dangerouslySetInnerHTML={{__html: "DOWNLOAD <br> AND USE"}}></h2>
-                        <Link 
-                            className="large-button"
-                            to={ `/${"download"}` }
-                        >
-                            DOWNLOAD .MD
-                        </Link>
-                    </PreFooter>
-                </>
-            }
-            {
-                isError && <p>{ error.message }</p>
+                            </div>
+                        </div>
+
+                        <PreFooter>
+                            <h2 dangerouslySetInnerHTML={{ __html: "DOWNLOAD <br> AND USE" }}></h2>
+                            <Link
+                                className="large-button"
+                                to={`/${"download"}`}
+                            >
+                                DOWNLOAD .MD
+                            </Link>
+                        </PreFooter>
+                    </>
+                )
             }
         </>
     )
